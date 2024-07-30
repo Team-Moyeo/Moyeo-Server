79a2dd41e0655acf59a62980b6234d2768ed77d4
KyungsooLee <jmtkd9196@gmail.com>
2024-07-30 11:54:10 +0900

    feat: #5 baseEntity 구현

--- src/main/java/com/otechdong/moyeo/MoyeoApplication.java
+++ src/main/java/com/otechdong/moyeo/MoyeoApplication.java
@@ -2,8 +2,9 @@ package com.otechdong.moyeo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class MoyeoApplication {
 
 	public static void main(String[] args) {
