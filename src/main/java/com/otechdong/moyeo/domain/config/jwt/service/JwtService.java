0ea38a83f5a13a571aa7adbb7254c1694eec082b
KyungsooLee <jmtkd9196@gmail.com>
2024-08-01 12:05:26 +0900

    feat: #21 EncryptedUserIdentifier로 Jwt 토큰 발급 로직 구현

--- src/main/java/com/otechdong/moyeo/domain/config/jwt/service/JwtService.java
+++ src/main/java/com/otechdong/moyeo/domain/config/jwt/service/JwtService.java
@@ -1,4 +1,6 @@
 package com.otechdong.moyeo.domain.config.jwt.service;
 
 public interface JwtService {
+    String createJwt(Long memberId, String role, String tokenType);
+    String createJwtFromEncryptedUserIdentifier(String encryptedUserIdentifier);
 }
--- src/main/java/com/otechdong/moyeo/domain/config/jwt/service/JwtServiceImpl.java
+++ src/main/java/com/otechdong/moyeo/domain/config/jwt/service/JwtServiceImpl.java
@@ -1,7 +1,10 @@
 package com.otechdong.moyeo.domain.config.jwt.service;
 
 import com.otechdong.moyeo.domain.config.jwt.dto.JwtProperties;
+import com.otechdong.moyeo.global.exception.RestApiException;
+import com.otechdong.moyeo.global.exception.errorCode.AuthErrorCode;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -9,6 +12,7 @@ import org.springframework.stereotype.Component;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
+import java.util.Date;
 
 @Component
 @RequiredArgsConstructor
@@ -19,6 +23,36 @@ public class JwtServiceImpl implements JwtService {
     @PostConstruct
     protected void init() {
         secretKey = new SecretKeySpec(jwtProperties.getJwt_secret().getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS512.key().build().getAlgorithm());
+    }
+
+
+    @Override
+    public String createJwt(Long memberId, String permissionRole, String tokenType) {
+        Long expiredTime = 0L;
+        if (tokenType == "refresh") {
+            expiredTime = jwtProperties.getRefresh_expired_time();
+        } else if (tokenType == "access") {
+            expiredTime = jwtProperties.getAccess_expired_time();
+        } else {
+            throw new RestApiException(AuthErrorCode.INVALID_TOKEN_TYPE);
+        }
+        return Jwts.builder()
+                .claim("tokenType", tokenType)
+                .claim("memberId", memberId)
+                .claim("permissionRole", permissionRole)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expiredTime))
+                .signWith(SignatureAlgorithm.HS512, secretKey)
+                .compact();
+    }
+
+    @Override
+    public String createJwtFromEncryptedUserIdentifier(String encryptedUserIdentifier) {
+        SecretKey encryptedUserIdentifierSecretKey = new SecretKeySpec((jwtProperties.getJwt_secret()+encryptedUserIdentifier).getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS512.key().build().getAlgorithm());
+        return Jwts.builder()
+                .setAudience("capple")
+                .signWith(SignatureAlgorithm.HS512, encryptedUserIdentifierSecretKey)
+                .compact();
     }
 
 }
