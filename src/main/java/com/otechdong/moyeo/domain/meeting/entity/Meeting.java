80b67c523fb9c0bbe9874f616d317ed5f809b287
KyungsooLee <jmtkd9196@gmail.com>
2024-08-03 17:56:54 +0900

    feat: #27 Meeting 엔티티 매핑

---
+++ src/main/java/com/otechdong/moyeo/domain/meeting/entity/Meeting.java
@@ -0,0 +1,57 @@
+package com.otechdong.moyeo.domain.meeting.entity;
+
+import com.otechdong.moyeo.domain.member.entity.PermissionRole;
+import com.otechdong.moyeo.global.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Entity
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Meeting extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "meeting_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column
+    private LocalDate startDate;
+
+    @Column
+    private LocalDate endDate;
+
+    @Column
+    private LocalTime startTime;
+
+    @Column
+    private LocalTime endTime;
+
+    @Column
+    private LocalDateTime fixedDate;
+
+//    @JoinColumn
+//    private List<CandidatedPlace> candidatedPlaces;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "place_id")
+    private Place fixedPlace;
+
+    @Column(nullable = false)
+    private LocalDateTime deadline;
+
+    @Column(nullable = false)
+    private Long numberOfPeople;
+
+    @Column(nullable = false)
+    private String inviteCode;
+}
