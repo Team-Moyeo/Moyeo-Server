5b34291b950673e6e419b7c2ff28d83f9ee39a23
KyungsooLee <jmtkd9196@gmail.com>
2024-08-03 19:01:30 +0900

    feat: #27 CandidatePlace 엔티티 매핑

---
+++ src/main/java/com/otechdong/moyeo/domain/place/entity/CandidatePlace.java
@@ -0,0 +1,38 @@
+package com.otechdong.moyeo.domain.place.entity;
+
+import com.otechdong.moyeo.domain.meeting.entity.Meeting;
+import com.otechdong.moyeo.domain.meeting.entity.Place;
+import com.otechdong.moyeo.domain.member.entity.Member;
+import com.otechdong.moyeo.domain.member.entity.Role;
+import com.otechdong.moyeo.global.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CandidatePlace extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "candidate_place_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "place_id")
+    private Place place;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "meeting_id")
+    private Meeting meeting;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "vote_id")
+//    private Vote vote;
+}
