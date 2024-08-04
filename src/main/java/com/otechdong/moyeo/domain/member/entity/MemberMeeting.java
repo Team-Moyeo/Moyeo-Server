83f473e3253b1bf8308422effa91978ac61653e2
KyungsooLee <jmtkd9196@gmail.com>
2024-08-04 21:51:24 +0900

    feat: #27 MemberMeeting 엔티티 매핑

---
+++ src/main/java/com/otechdong/moyeo/domain/member/entity/MemberMeeting.java
@@ -0,0 +1,31 @@
+package com.otechdong.moyeo.domain.member.entity;
+
+import com.otechdong.moyeo.domain.meeting.entity.Meeting;
+import com.otechdong.moyeo.domain.place.entity.Place;
+import com.otechdong.moyeo.global.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemberMeeting extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_meeting_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "meeting_id")
+    private Meeting meeting;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Column(nullable = false)
+    private Role role;
+}
