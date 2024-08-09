1b82c5a9ac2b44a8935a5a462c9e5a08f0d3917d
KyungsooLee <jmtkd9196@gmail.com>
2024-08-09 11:37:37 +0900

    chore: #35 Swagger 태그 설정 & Security 설정 변경

--- src/main/java/com/otechdong/moyeo/domain/meeting/controller/MeetingController.java
+++ src/main/java/com/otechdong/moyeo/domain/meeting/controller/MeetingController.java
@@ -7,16 +7,20 @@ import com.otechdong.moyeo.domain.meeting.entity.MeetingStatus;
 import com.otechdong.moyeo.domain.meeting.service.MeetingService;
 import com.otechdong.moyeo.domain.member.entity.Member;
 import com.otechdong.moyeo.global.common.BaseResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/meetings")
+@Tag(name = "모임 API", description = "모임 API 입니다.")
 public class MeetingController {
 
     private final MeetingService meetingService;
 
+    @Operation(summary = "모임 생성 API", description = "모임 생성 API 입니다.")
     @PostMapping
     public BaseResponse<MeetingResponse.MeetingCreate> createMeeting(
             @AuthenticationMember Member member,
@@ -34,6 +38,7 @@ public class MeetingController {
                 request.getDeadline()));
     }
 
+    @Operation(summary = "(상태별) 모임 조회 API", description = "(PENDING, CONFIRM, END) 모임 조회 API 입니다.")
     @GetMapping
     public BaseResponse<MeetingResponse.MeetingGetList> getMeetingsByMeetingStatus(
             @AuthenticationMember Member member,
@@ -42,6 +47,7 @@ public class MeetingController {
         return BaseResponse.onSuccess(meetingService.getMeetingsByMeetingStatus(member, meetingStatus));
     }
 
+    @Operation(summary = "모임 삭제 API", description = "모임 삭제 API 입니다.")
     @DeleteMapping("/{meetingId}")
     public BaseResponse<MeetingResponse.MeetingDelete> deleteMeeting(
             @AuthenticationMember Member member,
@@ -50,6 +56,7 @@ public class MeetingController {
         return BaseResponse.onSuccess(meetingService.deleteMeeting(member, meetingId));
     }
 
+    @Operation(summary = "모임 가입 API", description = "초대코드를 통한 모임 가입 API 입니다.")
     @PostMapping("/join/{inviteCode}")
     public BaseResponse<MeetingResponse.MeetingJoinWithInviteCode> joinMeetingWithInviteCode(
             @AuthenticationMember Member member,
@@ -58,6 +65,7 @@ public class MeetingController {
         return BaseResponse.onSuccess(meetingService.joinMeetingWithInviteCode(member, inviteCode));
     }
 
+    @Operation(summary = "초대코드 조회 API", description = "모임 초대코드 조회 API 입니다.")
     @GetMapping("/invite-code/{meetingId}")
     public BaseResponse<MeetingResponse.MeetingGetInviteCode> getInviteCode(
             @PathVariable(value = "meetingId") Long meetingId
@@ -65,6 +73,7 @@ public class MeetingController {
         return BaseResponse.onSuccess(meetingService.getInviteCode(meetingId));
     }
 
+    @Operation(summary = "모임 상세 조회 API", description = "모임 상세 조회 API 입니다.")
     @GetMapping("/{meetingId}")
     public BaseResponse<MeetingResponse.MeetingGetDetail> getMeetingDetail(
             @AuthenticationMember Member member,
@@ -73,6 +82,7 @@ public class MeetingController {
         return BaseResponse.onSuccess(meetingService.getMeetingDetail(member, meetingId));
     }
 
+    @Operation(summary = "투표 확정 API", description = "투표 확정 API 입니다.")
     @PostMapping("/{meetingId}/vote")
     public BaseResponse<MeetingResponse.MeetingVoteConfirm> voteConfirm(
             @AuthenticationMember Member member,
@@ -82,6 +92,7 @@ public class MeetingController {
         return BaseResponse.onSuccess(meetingService.voteConfirm(member, meetingId, request.getCandidateTimeIds(), request.getCandidatePlaceIds()));
     }
 
+    @Operation(summary = "재투표 API", description = "재투표 API 입니다.")
     @PostMapping("/{meetingId}/vote/update")
     public BaseResponse<MeetingResponse.MeetingVoteUpdate> voteUpdate(
             @AuthenticationMember Member member,
--- src/main/java/com/otechdong/moyeo/domain/member/controller/MemberController.java
+++ src/main/java/com/otechdong/moyeo/domain/member/controller/MemberController.java
@@ -6,22 +6,27 @@ import com.otechdong.moyeo.domain.member.dto.MemberResponse;
 import com.otechdong.moyeo.domain.member.entity.Member;
 import com.otechdong.moyeo.domain.member.service.MemberService;
 import com.otechdong.moyeo.global.common.BaseResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/members")
+@Tag(name = "멤버 API", description = "멤버 API 입니다.")
 public class MemberController {
 
     private final MemberService memberService;
 
+    @Operation(summary = "마이 프로필 조회 API", description = "마이 프로필 조회 API 입니다.")
     @GetMapping("/profile")
     public BaseResponse<MemberResponse.MemberGetMyProfile> getMyProfile(
             @AuthenticationMember Member member) {
         return BaseResponse.onSuccess(memberService.getMyProfile(member));
     }
 
+    @Operation(summary = "마이 프로필 업데이트 API", description = "마이 프로필 조회 API 입니다.")
     @GetMapping("/profile/update")
     public BaseResponse<MemberResponse.MemberUpdateMyProfile> updateMyProfile(
             @AuthenticationMember Member member,
@@ -29,6 +34,7 @@ public class MemberController {
         return BaseResponse.onSuccess(memberService.updateMyProfile(member, request));
     }
 
+    @Operation(summary = "모임 참여자 조회 API", description = "모임 참여자 조회 API 입니다.")
     @GetMapping("/meeting/{meetingId}")
     public BaseResponse<MemberResponse.MemberGetListByMeeting> getMembersByMeeting(
             @PathVariable(name = "meetingId") Long meetingId
